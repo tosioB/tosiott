a492b6424b92fb185ae75be3bf1ec7a3d1f35a48
kimdaesik <ds500033@naver.com>
2024-07-18 18:37:07 +0900

    계산기 만들기(미완) - 미구현 내용은 코드에 주석 처리

--- make_anything/calculator/script.js
+++ make_anything/calculator/script.js
@@ -1,64 +1,76 @@
-let display = document.querySelector('.display');
-let buttons = document.querySelectorAll('.button');
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
 
-let firstOperand; // 첫번째 반환값
-let secondOperand; // 두번째 반환값
-let operator; // 연산자
+let firstOperand = 0; // 첫번째 반환값
+let secondOperand = 0; // 두번째 반환값
+let operator = null; // 연산자
+let func = null; // 팡션
+let displayStatus = false; // 디스플레이 상태
 
-function calculate(a,b,c) {
-  value = eval(a+b+c);
-  firstOperand = value;
-  display.textContent = value;
-}
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    let numberClasses = button.classList.contains('number');
-    let operatorClasses = button.classList.contains('operator');
-    let btnValue = button.textContent;
-
-    if (numberClasses) { // number button click
-      if (firstOperand) {
-        if (display.textContent === firstOperand) {
-          display.textContent = btnValue;
+buttons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const numClass = btn.classList.contains('number');
+    const operClass = btn.classList.contains('operator');
+    const funcClass = btn.classList.contains('function');
+    const btnVal = btn.textContent;
+    
+    if (numClass) {
+      if (operator) {
+        if (displayStatus) {
+          secondOperand += btnVal;
+          secondOperand = Number(secondOperand);
+          display.textContent = secondOperand;
         } else {
-          display.textContent += btnValue;
+          secondOperand = btnVal;
+          secondOperand = Number(secondOperand);
+          display.textContent = secondOperand;
+          displayStatus = true;
         }
       } else {
-        if (display.textContent === '0') {
-          display.textContent = btnValue;
+        if (displayStatus) {
+          firstOperand += btnVal;
+          firstOperand = Number(firstOperand);
+          display.textContent = firstOperand;
         } else {
-          display.textContent += btnValue;
+          firstOperand = btnVal;
+          firstOperand = Number(firstOperand);
+          display.textContent = firstOperand;
+          displayStatus = true;
         }
       }
     }
 
-    if (operatorClasses) { // operator button click
-      firstOperand = display.textContent;
-      operator = btnValue;
-    }
-
-    switch(btnValue) {
-      case '=':
-        secondOperand = display.textContent;
-        calculate (firstOperand, operator, secondOperand);
-        break;
-      case 'C':
-        display.innerHTML = '0';
-        firstOperand = false;
-        secondOperand = false;
-        break;
-      case '.':
-        if (!display.textContent.includes('.')) {
-          display.textContent += btnValue;
+    if (operClass) {
+        operator = btnVal;
+        displayStatus = false;
+        switch(operator) {
+          case "+" :
+            firstOperand = firstOperand + secondOperand;
+            break;
+          case "-" :
+            firstOperand = firstOperand - secondOperand;
+            break;
+          case "*" :
+            firstOperand = firstOperand * secondOperand;
+            break;
+          case "/" :
+            firstOperand = firstOperand / secondOperand;
+            break;
+          case "%" :
+            // firstOperand = firstOperand / secondOperand;
+            console.log('아직 미구현')
+            break;
         }
-        break;
+        display.textContent = firstOperand;
     }
+    console.log(firstOperand,operator, secondOperand)
   });
 });
 
 
 
+// 1 + 2 + 3 - // 6이 나와야 하는데 0이 나옴
+// "c", "=", ".", "%", "±" 기능 구현X
 
 
 
@@ -89,39 +101,112 @@ buttons.forEach((button) => {
 
 
 
-// let numberBtns = document.querySelectorAll('.button.number');
-// let operatorBtns = document.querySelectorAll('.button.operator');
 
-// numberBtns.forEach((numBtn) => {
-//   numBtn.addEventListener('click', () => {
-//     let btnValue = numBtn.textContent;
-    
-//     if (operator) {
-//       if (secondOperand) {
-//         secondOperand += btnValue;
-//         display.textContent = secondOperand;
-//       } else {
-//         secondOperand = btnValue;
-//         display.textContent = secondOperand;
-//       }
-//     } else {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let display = document.querySelector('.display');
+// let buttons = document.querySelectorAll('.button');
+
+// let firstOperand; // 첫번째 반환값
+// let secondOperand; // 두번째 반환값
+// let operator; // 연산자
+
+// function calculate(a,b,c) {
+//   value = eval(a+b+c);
+//   firstOperand = value;
+//   display.textContent = value;
+// }
+
+// buttons.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     let numberClasses = button.classList.contains('number');
+//     let operatorClasses = button.classList.contains('operator');
+//     let btnValue = button.textContent;
+
+//     if (numberClasses) { // number button click
 //       if (firstOperand) {
-//         firstOperand += btnValue;
-//         display.textContent = firstOperand;
+//         if (display.textContent === firstOperand) {
+//           display.textContent = btnValue;
+//         } else {
+//           display.textContent += btnValue;
+//         }
 //       } else {
-//         firstOperand = btnValue;
-//         display.textContent = firstOperand;
+//         if (display.textContent === '0') {
+//           display.textContent = btnValue;
+//         } else {
+//           display.textContent += btnValue;
+//         }
 //       }
 //     }
-//     console.log(`첫번째 : ${firstOperand}`)
-//     console.log(`두번째 : ${secondOperand}`)
-//   });
-// })
 
-// operatorBtns.forEach((operBtn) => {
-//   operBtn.addEventListener('click', () => {
-//     let btnValue = operBtn.textContent;
-//     operator = btnValue;
-//     console.log(operator)
+//     if (operatorClasses) { // operator button click
+//       firstOperand = display.textContent;
+//       operator = btnValue;
+//     }
+
+//     switch(btnValue) {
+//       case '=':
+//         secondOperand = display.textContent;
+//         calculate (firstOperand, operator, secondOperand);
+//         break;
+//       case 'C':
+//         display.innerHTML = '0';
+//         firstOperand = false;
+//         secondOperand = false;
+//         break;
+//       case '.':
+//         if (!display.textContent.includes('.')) {
+//           display.textContent += btnValue;
+//         }
+//         break;
+//     }
 //   });
 // });
